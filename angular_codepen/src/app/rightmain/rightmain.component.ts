c80e94f2bc4e79b5cba2f3c8d28b12adf4511649
Sagar <sagar.arora198@gmail.com>
2024-02-26 10:16:25 +0530

    right-part done

--- angular_codepen/src/app/rightmain/rightmain.component.ts
+++ angular_codepen/src/app/rightmain/rightmain.component.ts
@@ -16,7 +16,45 @@ export class RightMain{
 
 
     }> = [
+        {
+            event_date:18,
+            event_month:"March",
+            event_heading:"Social Media",
+            event_location:"Willson Tech Park",
+        },
+        {
+            event_date:22,
+            event_month:"June",
+            event_heading:"Mobile Marketing",
+            event_location:"Willson Tech Park",
+        },
+
+
+
         
+
+    ]
+    Conversations:Array<{
+        friend_pfp:string,
+        friend_name:string
+    }> = [
+        {
+            friend_pfp:"https://i.postimg.cc/XNPtfdVs/member-1.png",
+            friend_name:"Alison Mina"
+        },
+        {
+            friend_pfp:"https://i.postimg.cc/4NhqByys/member-2.png",
+            friend_name:"Jackson Aston"
+        },
+        {
+            friend_pfp:"https://i.postimg.cc/FH5qqvkc/member-3.png",
+            friend_name:"Samona Rosea"
+        },
+        {
+            friend_pfp:"https://i.postimg.cc/Sx65bPcP/member-4.png",
+            friend_name:"Mike Pérez"
+        },
+
 
     ]
 }
